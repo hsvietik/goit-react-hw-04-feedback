2da5576d0bd611072fc30b0467ff039bf9d5adb1
Hanna Svietik <hsvietik@gmail.com>
2023-06-25 20:08:10 +0100

    class changed to function with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const options = { good, neutral, bad };
+  const onLeaveFeedback = item => {
+    switch (item) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        window.alert('There is no option');
+    }
   };
-  onLeaveFeedback = item => {
-    this.setState(prevState => ({
-      [item]: prevState[item] + 1,
-    }));
-  };
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, val) => {
-      return (acc += val);
-    }, 0);
-  };
-  countPositiveFeedbackPercentage = () => {
-    let positive = Object.entries(this.state).reduce((acc, [key, value]) => {
-      if (key === 'good') {
-        return (acc += value);
-      }
-      return acc;
-    }, 0);
-    let positivePersentage = Math.round(
-      (positive * 100) / this.countTotalFeedback()
-    );
-    return positivePersentage;
-  };
-  render() {
-    return (
-      <Section title="Please leave feedback">
-        <FeedbackOptions
-          options={Object.keys(this.state)}
-          onLeaveFeedback={this.onLeaveFeedback}
-        />
+  let total = good + neutral + bad;
 
-        <Statistics
-          statistics={this.state}
-          total={this.countTotalFeedback()}
-          positivePercentage={this.countPositiveFeedbackPercentage()}
-        />
-      </Section>
-    );
-  }
+  let positivePersentage = Math.round((good * 100) / total);
+
+  return (
+    <Section title="Please leave feedback">
+      <FeedbackOptions
+        options={Object.keys(options)}
+        onLeaveFeedback={onLeaveFeedback}
+      />
+
+      <Statistics
+        statistics={options}
+        total={total}
+        positivePercentage={positivePersentage}
+      />
+    </Section>
+  );
 }
